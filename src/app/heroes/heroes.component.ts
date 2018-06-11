c6f5e7fe5f0011f4a68044b72b8e3ef21c49aa4b
kamal <lalwanikamaldev@hotmail.com>
2018-06-11 12:02:25 +0530

    commit changes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../heroslist';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+//  hero:Hero = {
+//    id: 1,
+//    name: "Akshay"
+//  }
+
+heroes: Hero[];
+selectedHero: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+
+
+constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+getHeroes(): void{
+  this.heroService.getHeroes()
+  .subscribe(heroes1 => this.heroes = heroes1);
+}
+
+
+}
---
+++ src/app/heroslist.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1,name: 'Akshay Kuamr' },
+  { id: 2, name: 'Rajkumar Rao' },
+  { id: 3, name: 'Ranveer Singh Bhawnani' },
+  { id: 4, name: 'Nawazudin siddqi' },
+  { id: 5, name: 'Ranveer Kapoor' },
+  { id: 6, name: 'Vicky khushal' },
+  { id: 7, name: 'Ajay devgan' },
+  { id: 8, name: 'Amir Khan' },
+  { id: 9, name: 'Shahruk Khan' },
+  { id: 10, name: 'Salman Khan' }
+];
